a1ab0950cfd29fee57ccb8cb2cd658d02ed86898
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-08-31 12:50:56 -0700

    Added fields, getter/setters, ctr to Inventory, Location, ScenarioGenerator & created help menu fields, AB#320.

--- src/main/java/com/tlglearning/util/Inventory.java
+++ src/main/java/com/tlglearning/util/Inventory.java
@@ -1,4 +1,20 @@
 package com.tlglearning.util;
 
+import java.util.List;
+
 public class Inventory {
+    private List<String> backpack;
+
+    public Inventory(List<String> backpack) {
+        this.backpack = backpack;
+    }
+
+    public List<String> getBackpack() {
+        return backpack;
+    }
+
+    public void setBackpack(List<String> backpack, String item) {
+        this.backpack = backpack;
+        backpack.add(item);
+    }
 }
--- src/main/java/com/tlglearning/util/Menu.java
+++ src/main/java/com/tlglearning/util/Menu.java
@@ -1,4 +1,26 @@
 package com.tlglearning.util;
 
+import java.util.List;
+
 public class Menu {
+
+    public static String helpMenu(Location location, Inventory inventory, ScenarioGenerator scenario){
+        String currentLocation = "Current Location: " + location.getName();
+        String exitN = "Exit North: " + location.getExit1To();
+        String exitS = "Exit South: " + location.getExit2To();
+        String exitE = "Exit East: " + location.getExit3To();
+        String exitW = "Exit West: " + location.getExit4To();
+        String availableCMD = "Available Commands:\n" +
+                "move/go + <direction> will change your current location\n" +
+                "explore + <specific place> will inspect specific parts of a location\n" +
+                "get/pickup + <item name> will add the item to your backpack\n";
+        List<String> backpack = inventory.getBackpack();
+        String currentScenario = "Home Office: " + scenario.getOfficeLocation() +
+               "\n Pickup Location: " + scenario.getPickupLocation() +
+               "\n Delivery Location " + scenario.getDeliveryLocation() +
+               "\n Items needed to get on the road:\n" + scenario.getItemsNeeded();
+
+        return " ";
+    }
+
 }
