2e72529fa7b5576bae5b889c38e7418f7fb1c765
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-09-06 12:39:32 -0700

    Added conditionals to check if lockable locations are accessible, AB#345.

--- src/main/java/com/tlglearning/util/Player.java
+++ src/main/java/com/tlglearning/util/Player.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.function.IntBinaryOperator;
 
 import static com.tlglearning.util.JacksonParser.*;
 
 public class Player {
 
 
-    public void move(String current, String nextLocation, Location currentLocation){
+    public void move(String current, String nextLocation, Location currentLocation) {
         JsonNode locations;
         File locationJson = new File("src/main/resources/location.json");
         try {
@@ -18,11 +19,11 @@ public class Player {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String newLocation = locationFinder(current,nextLocation,locations);
-        if (newLocation == null ||newLocation.equals("null")){
+        String newLocation = locationFinder(current, nextLocation, locations);
+        if (newLocation == null || newLocation.equals("null")) {
             System.out.println("That location is invalid.");
             return;
-        }else {
+        } else {
             currentLocation.setLocationName(newLocation);
             currentLocation.setNorth(getDescription(newLocation, "north", locations));
             currentLocation.setSouth(getDescription(newLocation, "south", locations));
@@ -33,7 +34,7 @@ public class Player {
         }
     }
 
-    public void explore(String current, String exploreLocation, Location currentLocation){
+    public void explore(String current, String exploreLocation, Inventory backpack) {
         JsonNode locations;
         File locationJson = new File("src/main/resources/exploreLocation.json");
         try {
@@ -41,16 +42,25 @@ public class Player {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String newExploreLocation = locationFinder(current,exploreLocation,locations);
-        if (newExploreLocation == null){
+        String newExploreLocation = locationFinder(current, exploreLocation, locations);
+        if (newExploreLocation == null) {
             System.out.println("That location is not explorable.");
         }
-        if (newExploreLocation != null){
-            System.out.println(newExploreLocation);
+        if (newExploreLocation != null) {
+            if (exploreLocation.equals("cabinet") || exploreLocation.equals("closet") || exploreLocation.equals("locker")) {
+                boolean hasKey = backpack.getBackpack().contains("key");
+                if (hasKey) {
+                    System.out.println(newExploreLocation);
+                } else {
+                    System.out.println("You need the key in the warehouse desk to open this");
+                }
+            } else {
+                System.out.println(newExploreLocation);
+            }
         }
     }
 
-    public void get(String current, String item, Location currentLocation, Inventory backpack){
+    public void get(String current, String item, Location currentLocation, Inventory backpack) {
         JsonNode locations;
         File locationJson = new File("src/main/resources/items.json");
         try {
@@ -58,16 +68,23 @@ public class Player {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String newItem = locationFinder(current,item,locations);
-        if (newItem == null){
+        String newItem = locationFinder(current, item, locations);
+        if (newItem == null) {
             System.out.println("That item is invalid.");
         }
         if (item != null) {
-            System.out.println(newItem);
-            backpack.setBackpack(item);
-        }//do nothing
+            if (item.equals("coffee")) {
+                boolean hasKey = backpack.getBackpack().contains("thermos");
+                if (hasKey) {
+                    System.out.println(newItem);
+                    backpack.setBackpack(item);
+                } else {
+                    System.out.println("You need something to put coffee in, grab your thermos from your locker");
+                }
+            } else {
+                System.out.println(newItem);
+                backpack.setBackpack(item);
+            }//do nothing
+        }
     }
-
-
-
 }
