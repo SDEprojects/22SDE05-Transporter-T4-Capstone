010f64fb717e85a9dcf234a79773c044bf13d154
KitaGaither <lakitgai@amazon.com>
2022-09-16 00:55:34 -0700

    Updated yaml file

--- src/test/com/tlglearning/util/InventoryTest.java
+++ src/test/com/tlglearning/util/InventoryTest.java
@@ -2,24 +2,24 @@ package com.tlglearning.util;
 
 import org.junit.jupiter.api.Test;
 
-
 import java.util.ArrayList;
-import java.util.function.BooleanSupplier;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.testng.Assert.assertTrue;
 
 class InventoryTest {
     private final ArrayList<String> backpack = new ArrayList<>();
+
     @Test
     void addItemShouldAddItemToBackPack() {
         assertTrue(true, String.valueOf(backpack.add("Key, logbook, soda")));
     }
+
     @Test
-    void getBackpackShouldShowContentOfBackpack(){
-       backpack.add("UsMap, Compass, Coffee");
+    void getBackpackShouldShowContentOfBackpack() {
+        backpack.add("UsMap, Compass, Coffee");
         assertTrue(backpack.contains("UsMap, Compass, Coffee"), String.valueOf(getBackpack()));
     }
+
     public ArrayList<String> getBackpack() {
         return backpack;
     }
